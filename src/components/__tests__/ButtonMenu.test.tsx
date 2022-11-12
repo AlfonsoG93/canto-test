2fc21ecf55bff9637ae19f9ea766042db3b62478
Alfonso Moncada <alfonso.moncada@osapiens.com>
2022-11-12 11:01:34 +0100

    resturcture test

--- src/components/__tests__/ButtonMenu.test.tsx
+++ src/components/__tests__/ButtonMenu.test.tsx
@@ -7,17 +7,6 @@ import ButtonMenu from "../ButtonMenu";
 const mockedButtonAction = jest.fn();
 
 
-test.only("only the reset button shows when approve is clicked", async () => {
-	render(<ButtonMenu checked={false} setApproval={mockedButtonAction}/>);
-	
-	const ApproveButton = screen.getByText("Approve");
-	fireEvent.click(ApproveButton as HTMLElement);
-	
-	expect(screen.queryByTestId("approve-button")).not.toBeInTheDocument();
-	expect(screen.queryByTestId("reset-button")).toBeInTheDocument();
-	screen.debug();
-});
-
 describe("Test ButtonMenu component", () => {
 	
 	beforeEach(() => {
@@ -42,26 +31,6 @@ describe("Test ButtonMenu component", () => {
 		const ResetButton = screen.queryByTestId("reset-button");
 		expect(ResetButton).not.toBeInTheDocument();
 		
-	});
-	test.skip("only the reset button shows when approve is clicked", async () => {
-		
-		
-		userEvent.click(screen.queryByTestId("approve-button") as HTMLElement);
-		
-		expect(screen.queryByTestId("approve-button")).not.toBeInTheDocument();
-		expect(screen.queryByTestId("reset-button")).toBeInTheDocument();
-	});
-	
-	test.skip("only the reset button shows when reject is clicked", () => {
-		
-		const ApproveButton = screen.getByTestId("approve-button");
-		const RejectButton = screen.getByTestId("reject-button");
-		const ResetButton = screen.getByTestId("reset-button");
-		
-		
-		expect(ApproveButton).not.toBeInTheDocument();
-		expect(RejectButton).not.toBeInTheDocument();
-		expect(ResetButton).toBeInTheDocument();
 	});
 	
 });
